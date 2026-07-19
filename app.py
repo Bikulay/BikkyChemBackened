73db552d0f72eefaad3ed37ed9e3579a93c9530b
Bikulay <bikky2gurung@gmail.com>
2026-07-19 19:32:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 # Load Gemini model
-model = genai.GenerativeModel("models/gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-pro")
 
 # Create Flask app
 app = Flask(__name__)
